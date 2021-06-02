f1c6807d63d6cb6e0ac7fa37cc0fa6efd3acea67
AzaharaGV <azahara1359@gmail.com>
2021-06-02 20:30:56 +0200

    FORMdESIGN

---
+++ my-project/src/components/Card/Main/Form-design.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+function FormDesign() {
+  return (
+    <fieldset className="design">
+      <div className="fieldset__button">
+        <div className="fieldset__button--title">
+          <i className="far fa-object-ungroup"></i>
+          <h2>Diseña</h2>
+        </div>
+        <i className="fas fa-chevron-down fa-chevron-up js-designButton"></i>
+      </div>
+      <div className="js-design">
+        <div className="design__expand">
+          <h3>Colores</h3>
+
+          <div className="colors__wrapper js_generalcolor">
+            <label className="label" htmlFor="palette1">
+              <input
+                type="radio"
+                name="palette"
+                id="palette1"
+                value="1"
+                checked
+                className="js-palette1"
+              />
+              <ul className="palette__wrapper">
+                <li className="color palette1__color1"></li>
+                <li className="color palette1__color2"></li>
+                <li className="color palette1__color3"></li>
+              </ul>
+            </label>
+
+            <label className="label" htmlFor="palette2">
+              <input
+                type="radio"
+                name="palette"
+                id="palette2"
+                value="2"
+                className="js-palette2"
+              />
+              <ul className="palette__wrapper">
+                <li className="color palette2__color1"></li>
+                <li className="color palette2__color2"></li>
+                <li className="color palette2__color3"></li>
+              </ul>
+            </label>
+
+            <label className="label" htmlFor="palette3">
+              <input
+                type="radio"
+                name="palette"
+                id="palette3"
+                value="3"
+                className="js-palette3"
+              />
+              <ul className="palette__wrapper">
+                <li className="color palette3__color1"></li>
+                <li className="color palette3__color2"></li>
+                <li className="color palette3__color3"></li>
+              </ul>
+            </label>
+          </div>
+        </div>
+      </div>
+    </fieldset>
+  );
+}
+export { FormDesign };
