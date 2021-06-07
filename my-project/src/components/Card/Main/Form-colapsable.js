443ce3d26e9d102efafe19125cf735c66f73a390
parnasos <lauragraizer@gmail.com>
2021-06-07 20:51:38 +0200

    Trying collapsable

--- my-project/src/components/Card/Main/Form-colapsable.js
+++ my-project/src/components/Card/Main/Form-colapsable.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../../../stylesheets/layout/_collapsable.scss";
+import "../../../stylesheets/layout/_form.scss";
 
 class Collapsable extends React.Component {
   constructor(props) {
@@ -6,23 +8,36 @@ class Collapsable extends React.Component {
     this.state = {
       isOpen: false,
     };
-  }
-  this.handleClick=this.handleClick.bind(this)
- 
-  handleClick (){
-      this.setState({
-          isOpen:!this.state.isOpen;
 
-      })
+    this.handleClick = this.handleClick.bind(this);
   }
-  render(){
-    console.log(this.state)
-    const open =this.state.isOpen?'open':'none';
-    
-    return(
-          
-      )
+  handleClick() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+  render() {
+    console.log(this.state);
+    const openClassName = this.state.isOpen ? "open" : "none";
+    // const openText = this.state.isOpen ? "Abierto" : "Cerrado";
+    return (
+      <>
+        <div className="fieldset__button">
+          <div className="fieldset__button--title">
+            <i className={this.props.icon}></i>
+            <h2>{this.props.title}</h2>
+          </div>
+          <i
+            className="fas fa-chevron-down fa-chevron-up js-designButton"
+            onClick={this.handleClick}
+          ></i>
+        </div>
+        <div className="js-design">
+          <div className={`design__expand ${openClassName}`}></div>
+        </div>
+      </>
+    );
   }
 }
 
-export {Collapsable}
+export { Collapsable };
--- my-project/src/components/Card/Main/Form_design.js
+++ my-project/src/components/Card/Main/Form_design.js
@@ -1,66 +1,59 @@
+import { Collapsable } from "./Form-colapsable";
+import "../../../stylesheets/layout/_collapsable.scss";
+
 function Form_design() {
   return (
     <fieldset className="design">
-      <div className="fieldset__button">
-        <div className="fieldset__button--title">
-          <i className="far fa-object-ungroup"></i>
-          <h2>Diseña</h2>
+      <Collapsable title="Diseña" icon="far fa-object-ungroup">
+        <h3>Colores</h3>
+        <div className="colors__wrapper js_generalcolor">
+          <label className="label" htmlFor="palette1">
+            <input
+              type="radio"
+              name="palette"
+              id="palette1"
+              value="1"
+              checked
+              className="js-palette1"
+            />
+            <ul className="palette__wrapper">
+              <li className="color palette1__color1"></li>
+              <li className="color palette1__color2"></li>
+              <li className="color palette1__color3"></li>
+            </ul>
+          </label>
+
+          <label className="label" htmlFor="palette2">
+            <input
+              type="radio"
+              name="palette"
+              id="palette2"
+              value="2"
+              className="js-palette2"
+            />
+            <ul className="palette__wrapper">
+              <li className="color palette2__color1"></li>
+              <li className="color palette2__color2"></li>
+              <li className="color palette2__color3"></li>
+            </ul>
+          </label>
+
+          <label className="label" htmlFor="palette3">
+            <input
+              type="radio"
+              name="palette"
+              id="palette3"
+              value="3"
+              className="js-palette3"
+            />
+            <ul className="palette__wrapper">
+              <li className="color palette3__color1"></li>
+              <li className="color palette3__color2"></li>
+              <li className="color palette3__color3"></li>
+            </ul>
+          </label>
         </div>
-        <i className="fas fa-chevron-down fa-chevron-up js-designButton"></i>
-      </div>
-      <div className="js-design">
-        <div className="design__expand">
-          <h3>Colores</h3>
-
-          <div className="colors__wrapper js_generalcolor">
-            <label className="label" htmlFor="palette1">
-              <input
-                type="radio"
-                name="palette"
-                id="palette1"
-                value="1"
-                checked
-                className="js-palette1"
-              />
-              <ul className="palette__wrapper">
-                <li className="color palette1__color1"></li>
-                <li className="color palette1__color2"></li>
-                <li className="color palette1__color3"></li>
-              </ul>
-            </label>
-
-            <label className="label" htmlFor="palette2">
-              <input
-                type="radio"
-                name="palette"
-                id="palette2"
-                value="2"
-                className="js-palette2"
-              />
-              <ul className="palette__wrapper">
-                <li className="color palette2__color1"></li>
-                <li className="color palette2__color2"></li>
-                <li className="color palette2__color3"></li>
-              </ul>
-            </label>
-
-            <label className="label" htmlFor="palette3">
-              <input
-                type="radio"
-                name="palette"
-                id="palette3"
-                value="3"
-                className="js-palette3"
-              />
-              <ul className="palette__wrapper">
-                <li className="color palette3__color1"></li>
-                <li className="color palette3__color2"></li>
-                <li className="color palette3__color3"></li>
-              </ul>
-            </label>
-          </div>
-        </div>
-      </div>
+      </Collapsable>
     </fieldset>
   );
 }
