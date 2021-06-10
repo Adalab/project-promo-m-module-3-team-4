0b202783c348b5a05814253ddfda5050f62e4da6
Lorellana21 <lorellana.ordonez@gmail.com>
2021-06-10 21:48:24 +0200

    add events to form-fill and input

--- src/components/Card/CardGenerator.js
+++ src/components/Card/CardGenerator.js
@@ -5,9 +5,32 @@ import { Form } from "./Main/Form";
 import { useState } from "react";
 
 function CardGenerator() {
+  const [palette, setPalette] = useState("1");
   const [name, setName] = useState("");
   const [job, setJob] = useState("");
   const [image, setImage] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+  const [github, setGithub] = useState("");
+
+  const handleInput = (inputName, inputValue) => {
+    if (inputName === "palette") {
+      setPalette(inputValue);
+    } else if (inputName === "name") {
+      setName(inputValue);
+    } else if (inputName === "job") {
+      setJob(inputValue);
+    } else if (inputName === "email") {
+      setEmail(inputValue);
+    } else if (inputName === "phone") {
+      setPhone(inputValue);
+    } else if (inputName === "linkedin") {
+      setLinkedin(inputValue);
+    } else if (inputName === "github") {
+      setGithub(inputValue);
+    }
+  };
 
   const updateAvatar = (image) => {
     setImage(image);
@@ -16,8 +39,29 @@ function CardGenerator() {
     <div className="page">
       <Header />
       <div className="profile__wrapper">
-        <Preview image={image} />
-        <Form image={image} updateAvatar={updateAvatar} />
+        <Preview
+          palette={palette}
+          name={name}
+          job={job}
+          email={email}
+          phone={phone}
+          linkedin={linkedin}
+          github={github}
+          // handleReset={handleReset}
+          image={image}
+        />
+        <Form
+          handleInput={handleInput}
+          palette={palette}
+          name={name}
+          job={job}
+          email={email}
+          phone={phone}
+          linkedin={linkedin}
+          github={github}
+          image={image}
+          updateAvatar={updateAvatar}
+        />
 
         {/* <div className="tooltipFill js-tooltipFill none">
         <p className="tooltipParagraph">
