796cc811311efe897c76eaa9f17538b5e21fa348
parnasos <lauragraizer@gmail.com>
2021-06-29 19:08:02 +0200

    Link to card

--- web/src/components/Card/Main/FormShare.js
+++ web/src/components/Card/Main/FormShare.js
@@ -44,7 +44,9 @@ function FormShare(props) {
           {cardURL !== "" ? (
             <div className="none card--created">
               <h3 className="none">La tarjeta ha sido creada:</h3>
-              <a className="js-url">{cardURL}</a>
+              <a className="js-url" href={cardURL}>
+                Link para acceder
+              </a>
               <a className="none share__button--twitter js-aTwitter">
                 Compartir en Twitter
               </a>
